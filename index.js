0fd1ce36effdc9316b5c679cebdcc87f544185ee
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-10-22 11:01:10 +0700

    server menggunakan Express dan koneksi ke database MySQL

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+let mysql = require("mysql2");
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    port: '3309',
+    password: '@Aiszr131004',
+    database: 'mahasiswa'
+});
